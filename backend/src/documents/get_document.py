9955db3ec98f429f12f7934ebd5a23930e69f79d
saquino0827 <saquino@flexion.us>
2025-05-12 14:50:22 -0500

    Update get_document to a filled DocumentItem if found and address unit tests accordingly

--- backend/src/documents/get_document.py
+++ backend/src/documents/get_document.py
@@ -12,9 +12,8 @@ def get_document(
     if document_info is None:
         return None, None, None
 
-    document_item = DocumentItem(**document_info)
-    remote_storage_url = document_item.document_url
+    remote_storage_url = document_info.document_url
     storage_access_url = cloud_storage.access_url(remote_storage_url)
     document_data = cloud_storage.get_file(remote_storage_url)
 
-    return document_item, storage_access_url, document_data
+    return document_info, storage_access_url, document_data
