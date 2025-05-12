51b71215a3bea79b7a7f787a8974b10371fbd96e
halprin <halprin@users.noreply.github.com>
2025-05-12 15:27:20 -0600

    Remove CSV metadata from data

--- ui/src/utils/downloadPageController.js
+++ ui/src/utils/downloadPageController.js
@@ -1,5 +1,5 @@
 export function generateCsvData(extractedData) {
-  let csvData = 'data:text/csv;charset=utf-8,Field,Value\n';
+  let csvData = 'Field,Value\n';
 
   const sortedEntries = Object.entries(extractedData).sort(([keyA], [keyB]) =>
     keyA.localeCompare(keyB, undefined, { numeric: true })
