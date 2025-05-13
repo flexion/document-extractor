bb141e68d4aea024b4ca25143af4ccc0218255ca
halprin <halprin@users.noreply.github.com>
2025-05-13 11:43:42 -0600

    Swap jest for vitest

--- ui/src/utils/downloadPageController.test.ts
+++ ui/src/utils/downloadPageController.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it } from 'vitest';
 
 import { generateCsvData } from './downloadPageController';
 import { ExtractedData } from './types';
